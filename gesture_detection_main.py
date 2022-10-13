e6120ddd8be48d3143cc16e23c1186cb71b9e3ff
Amar Khanshali <akhanshali2328@sdsu.edu>
2022-10-13 11:53:18 -0700

    added base files

---
+++ gesture_detection_main.py
@@ -0,0 +1,10 @@
+#
+# CS530 Systems Programming
+# Gesture-Based Home Control
+# Date Created : Oct 12, 2022
+# 
+
+
+if __name__ == '__main__':
+    print("This is the main file, not necessary to do anything here")
+    print("Copy this method and put in your file to test your code")
