75c1a2cc1cad1741c779e355770ee06fdf92b0bf
Edgar Navarro <3dgarnavarro@gmail.com>
2022-11-21 22:45:49 -0800

    Made secrets default to user input. This is more friendly for the repo.

--- nest_secrets.py
+++ nest_secrets.py
@@ -1,11 +1,11 @@
 # comment this line if you are using your own data
-from nest_secrets_private import PROJECT_ID, DEVICE_ID, CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN
+# from nest_secrets_private import PROJECT_ID, DEVICE_ID, CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN
 
 # add your own data
 # information needed to access NEST API
 # comment these lines if you are using your own data
-# PROJECT_ID = ""
-# DEVICE_ID = ""
-# CLIENT_ID = ""
-# CLIENT_SECRET = ""
-# REFRESH_TOKEN = ""
+PROJECT_ID = ""
+DEVICE_ID = ""
+CLIENT_ID = ""
+CLIENT_SECRET = ""
+REFRESH_TOKEN = ""
