b932fd06fce67962c8df1d627466ee4c845c125f
thubular <63025159+thubular@users.noreply.github.com>
2022-11-21 16:03:33 -0800

    added spotify_secrets.py template

---
+++ spotify_secrets.py
@@ -0,0 +1,10 @@
+# comment this line if you are using your own data
+from spotify_secrets_private import DEVICE_ID, CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN
+
+# add your own data
+# information needed to access Spotify API
+# comment these lines i you are using your own data
+# DEVICE_ID = ""
+# CLIENT_ID = ""
+# CLIENT_SECRET = ""
+# REFRESH_TOKEN = ""
